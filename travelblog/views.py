215b466569179335f0255f93cfaceab9b5e21147
Delfinistkata <elidimitrovadkit@gmail.com>
2023-07-08 16:12:58 +0000

    Add views for the Post model
    Add views for the Post model. Create 3 templates. Connect the views to the urls for the Post.  Create css folder and css file. Add basic content on the templates for the post.  Add basic styling on the css file for the Post

--- travelblog/views.py
+++ travelblog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    """A class-based view that displays a list of published posts.
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
