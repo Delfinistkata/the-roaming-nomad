0f2e2be73adb43068db65687967f698ef4917480
Delfinistkata <elidimitrovadkit@gmail.com>
2023-07-08 21:10:38 +0000

    Add views for the detailed post
    Add the views for the detailed post on a single page. Connect the urls. Add basic content to the post detail html file. Connect the href in the index.htl

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostList
+from .views import PostList, PostDetailView
 
 urlpatterns = [
-    path("", PostList.as_view(), name="home"),
+    path('', PostList.as_view(), name='home'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- travelblog/views.py
+++ travelblog/views.py
@@ -10,3 +10,8 @@ class PostList(ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
