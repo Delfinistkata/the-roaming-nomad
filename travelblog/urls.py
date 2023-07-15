dec0da5b23bf1d8f32af2047b4e8aa552c65b57a
Delfinistkata <elidimitrovadkit@gmail.com>
2023-07-15 14:55:11 +0000

    Add path for delete post

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path
-from .views import PostList, PostDetailView, CreatePostView, EditPostView
+from .views import (
+    PostList,
+    PostDetailView,
+    CreatePostView,
+    EditPostView,
+    DeletePostView
+)
+
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
-    path('templates/create_post/', CreatePostView.as_view(), name='create_post'),
-    path('templates/edit_post/<slug:slug>/', EditPostView.as_view(), name='edit_post'),
+    path(
+        'templates/create_post/',
+        CreatePostView.as_view(),
+        name='create_post'
+    ),
+    path(
+        'templates/edit_post/<slug:slug>/',
+        EditPostView.as_view(),
+        name='edit_post'
+    ),
+    path(
+        'templates/<slug:slug>/remove',
+        DeletePostView.as_view(),
+        name='delete_post'
+    ),
 ]
