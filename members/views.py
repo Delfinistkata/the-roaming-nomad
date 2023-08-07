debe46f166ed2f1b6815f16376f2254e8c61ddaa
Delfinistkata <elidimitrovadkit@gmail.com>
2023-08-07 17:27:09 +0000

    Add views for the 2 profile pages

--- members/views.py
+++ members/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from travelblog.models import UserProfile
 from .forms import SignUpForm, EditProfileForm, PasswordChangedFrom
 
+
+class EditProfilePageView(generic.UpdateView):
+    model = UserProfile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_picture', 'date_of_birth', 'website',
+    'facebook_profile', 'twitter_profile',
+    'linkedin_profile', 'instagram_profile', 'youtube_profile', 'phone_number', 'address',
+    'location', 'interests_or_hobbies', 'subscribed_categories']
+    success_url = reverse_lazy('home')
+
+
+class ShowProfilePageView(DetailView):
+    model = UserProfile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(UserProfile, id=self.kwargs['pk'])
+        context['page_user'] = page_user
+        return context
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangedFrom
