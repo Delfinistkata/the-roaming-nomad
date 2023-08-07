4e697bc988e2a74fc8697f9916dd74403beb3362
Delfinistkata <elidimitrovadkit@gmail.com>
2023-08-07 17:26:38 +0000

    Add path for 2 profile pages

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_user_profile_page'),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page')
 ]
