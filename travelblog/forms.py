a71dfd80ebacb9a033388790f8e6a9c16fbbe35f
Delfinistkata <elidimitrovadkit@gmail.com>
2023-09-13 19:32:56 +0000

    Add form for comment

--- travelblog/forms.py
+++ travelblog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -72,3 +72,23 @@ class CategoryEditForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['category_name']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = (
+            'author',
+            'body_content',
+        )
+
+        widgets = {
+            'author': forms.TextInput(attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Fill in your name'
+                    }),
+            'body_content': forms.Textarea(attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Comment here',
+                }),
+        }
