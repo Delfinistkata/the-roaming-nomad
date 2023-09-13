1ba7b1d3b90906037c41f9205b581661fb04bfc3
Delfinistkata <elidimitrovadkit@gmail.com>
2023-09-13 19:42:09 +0000

    Add url for user profile page

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView, CreateProfilePageView
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_user_profile_page'),
-    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page')
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
+    path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page')
 ]
