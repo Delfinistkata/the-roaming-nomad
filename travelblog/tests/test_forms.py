556726fa6ba54552a5095be21506fe92d71fddf2
Delfinistkata <elidimitrovadkit@gmail.com>
2023-09-18 18:38:46 +0000

    Add comments

--- travelblog/tests/test_forms.py
+++ travelblog/tests/test_forms.py
@@ -1,9 +1,19 @@
+"""
+Module containing test cases for various
+forms used in the travelblog application.
+"""
 from django.test import TestCase
 from travelblog.forms import PostForm, EditForm, CategoryEditForm, CommentForm
 
 
 class PostFormTest(TestCase):
+    """
+    Tests cases for the PostForm.
+    """
     def test_valid_post_form(self):
+        """
+        Tests the validity of the PostForm with valid data.
+        """
         data = {
             'title': 'Test Title',
             'title_tag': 'Test Tag',
@@ -20,14 +30,22 @@ class PostFormTest(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_invalid_post_form(self):
-        # Tests with missing required fields or invalid data
+        """
+        Tests the validity of the PostForm with missing or invalid data.
+        """
         data = {}
         form = PostForm(data=data)
         self.assertFalse(form.is_valid())
 
 
 class EditFormTest(TestCase):
+    """
+    Tests cases for the EditForm.
+    """
     def test_valid_edit_form(self):
+        """
+        Tests the validity of the EditForm with valid data.
+        """
         data = {
             'title': 'Updated Title',
             'title_tag': 'Updated Tag',
@@ -41,14 +59,22 @@ class EditFormTest(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_invalid_edit_form(self):
-        # Tests with missing required fields or invalid data
+        """
+        Test the validity of the EditForm with missing or invalid data.
+        """
         data = {}
         form = EditForm(data=data)
         self.assertFalse(form.is_valid())
 
 
 class CategoryEditFormTest(TestCase):
+    """
+    Tests cases for the CategoryEditForm.
+    """
     def test_valid_category_edit_form(self):
+        """
+        Test the validity of the CategoryEditForm with valid data.
+        """
         data = {
             'category_name': 'Updated Category Name',
         }
@@ -56,14 +82,22 @@ class CategoryEditFormTest(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_invalid_category_edit_form(self):
-        # Tests with missing required fields or invalid data
+        """
+        Test the validity of the CategoryEditForm with missing or invalid data.
+        """
         data = {}
         form = CategoryEditForm(data=data)
         self.assertFalse(form.is_valid())
 
 
 class CommentFormTest(TestCase):
+    """
+    Tests cases for the CommentForm.
+    """
     def test_valid_comment_form(self):
+        """
+        Tests the validity of the CommentForm with valid data.
+        """
         data = {
             'author': 'John Doe',
             'body_content': 'Test comment content',
@@ -72,7 +106,9 @@ class CommentFormTest(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_invalid_comment_form(self):
-        # Tests with missing required fields or invalid data
+        """
+        Test the validity of the CommentForm with missing or invalid data.
+        """
         data = {}
         form = CommentForm(data=data)
         self.assertFalse(form.is_valid())
