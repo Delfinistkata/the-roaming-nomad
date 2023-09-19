f72e1e6a8ff264c767e348eec3e757c07d86fe47
Delfinistkata <elidimitrovadkit@gmail.com>
2023-09-19 20:26:51 +0000

    Add name to the PasswordsChangeView path

--- members/urls.py
+++ members/urls.py
@@ -18,11 +18,11 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(
-        template_name='registration/change-password.html')),
+        template_name='registration/change-password.html'), name='password_change'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(),
          name='show_user_profile_page'),
     path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(),
          name='edit_profile_page'),
     path('create_profile_page/', CreateProfilePageView.as_view(),
-         name='create_profile_page')
+         name='create_profile_page'),
 ]
