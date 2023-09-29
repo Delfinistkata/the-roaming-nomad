8587a5c1dcb3602a6c1c136a5115f95c01499e2a
Delfinistkata <elidimitrovadkit@gmail.com>
2023-09-29 20:59:24 +0000

    Add path on new lines

--- members/urls.py
+++ members/urls.py
@@ -17,8 +17,13 @@ from .views import (
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
-    path('password/', PasswordsChangeView.as_view(
-        template_name='registration/change-password.html'), name='password_change'),
+    path(
+        'password/',
+        PasswordsChangeView.as_view(
+            template_name='registration/change-password.html'
+        ),
+        name='password_change'
+    ),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(),
          name='show_user_profile_page'),
     path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(),
