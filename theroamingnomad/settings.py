e1e9d3e4650c074725f0992fa78c3b1abff6dbd7
Delfinistkata <elidimitrovadkit@gmail.com>
2023-09-29 21:21:01 +0000

    Change debug to false

--- theroamingnomad/settings.py
+++ theroamingnomad/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEVELOPMENT')
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'the-roaming-nomad-travel-blog-f5d807b27cbd.herokuapp.com',
