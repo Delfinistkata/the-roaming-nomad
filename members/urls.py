30c6d01e177f9874e89773cb154ffba5fd0b00c8
Delfinistkata <elidimitrovadkit@gmail.com>
2023-10-04 20:24:08 +0000

    Change url for edit profile page

--- members/urls.py
+++ members/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     ),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(),
          name='show_user_profile_page'),
-    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(),
+    path('edit_profile_page/', EditProfilePageView.as_view(),
          name='edit_profile_page'),
     path('create_profile_page/', CreateProfilePageView.as_view(),
          name='create_profile_page'),
