9339383bd1282a9c8f8bdf26dae258061314160e
Delfinistkata <elidimitrovadkit@gmail.com>
2023-10-05 00:15:00 +0000

    Add cdeditor to body content

--- travelblog/forms.py
+++ travelblog/forms.py
@@ -5,6 +5,7 @@ allowing users to create and edit posts,
 manage categories, and add comments to posts.
 """
 from django import forms
+from ckeditor.widgets import CKEditorWidget
 from .models import Post, Category, Comment
 
 
@@ -40,10 +41,7 @@ class PostForm(forms.ModelForm):
                     }),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'categories': forms.CheckboxSelectMultiple(),
-            'body_content': forms.Textarea(attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Text goes here',
-                }),
+            'body_content': CKEditorWidget(),
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
         }
 
@@ -82,10 +80,7 @@ class EditForm(forms.ModelForm):
                     }),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'categories': forms.CheckboxSelectMultiple,
-            'body_content': forms.Textarea(attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Text goes here',
-                }),
+            'body_content': CKEditorWidget(),
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
         }
 
